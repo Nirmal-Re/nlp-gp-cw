90ff68e6f77874abb87b5f33e0b47de77c7af526
Nirmal Bhandari <bhdnirmal99@gmail.com>
2024-05-21 20:05:49 +0100

    working app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
-from transformers import BertForTokenClassification,BertTokenizer
+from flask import Flask, render_template, request
+from transformers import BertForTokenClassification,BertTokenizerFast
 import torch
-import numpy as np
+from collections import Counter
+
     
 def load_model_and_tokenizer(path):
-    tokenizer = BertTokenizer.from_pretrained(path)
+    tokenizer = BertTokenizerFast.from_pretrained(path)
     model = BertForTokenClassification.from_pretrained(path)
     return tokenizer, model
 
@@ -14,16 +15,42 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("index.html")
 
-@app.route("/predict")
+@app.route('/predict', methods=['POST'])
 def predict():
-    text = ["EU", "Nepal"]
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, is_split_into_words=True, return_offsets_mapping=True)
+    message = request.form['message']
+    text = message.split(" ")
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, is_split_into_words=True)
     outputs = model(**inputs)
     predictions = torch.argmax(outputs.logits, dim=-1)
-    print(predictions)
-    return "predicted"
+    true_predictions = [model.config.id2label[prediction] for prediction in predictions.tolist()[0]]
+    tokens = [tokenizer.convert_ids_to_tokens(id) for id in inputs['input_ids'].tolist()][0]
+
+    original_tokens = []
+    one_to_one_predictions = []
+    for token, predicted in zip(tokens, true_predictions):
+        if token in ['[CLS]', '[SEP]']:
+            continue
+
+        if token.startswith("##"):
+            original_tokens[-1] += token[2:]
+            one_to_one_predictions[-1].append(predicted)
+        else:
+            original_tokens.append(token)
+            one_to_one_predictions.append([predicted])
+    results = []
+    print(original_tokens)
+    for i, pred in enumerate(one_to_one_predictions):
+        if len(pred) == 1:
+            value = f"{original_tokens[i]}: {pred[0]}"
+            results.append(value)
+        if len(pred) > 1:
+             most_common_pred = Counter(pred).most_common(1)[0][0]
+             value = f"{original_tokens[i]}: {most_common_pred}"
+             results.append(value)
+
+    return {"results": results}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='localhost', port=5000)
